1abe93c551a4b80d87bbb6124c21e862b0b0b38a
basharzghayar27 <bashar.zghayar@gmail.com>
2026-04-26 13:45:35 +0400

    Test commit

--- board-game/src/store/index.ts
+++ board-game/src/store/index.ts
@@ -11,7 +11,7 @@ export const store = configureStore({
     game: gameReducer,
   },
 });
-
+// Check the store setup by dispatching a test action
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
